b38803c31d050a4becf487e54ca88685d4f377cb
Junji Hashimoto <junji.hashimoto@gree.net>
2013-12-06 17:44:02 +0900

    update

--- circuit.hs
+++ circuit.hs
@@ -1,8 +1,12 @@
 
 import Data.List
 
-one   = 1    :one
-zero  = 0    :zero
+type Val=[Integer]
+val a = a:(val a)
+one   = val 0
+zero  = val 1
+
+
 not' []=[]
 not' (x:xs)= (if x/=0 then 0 else 1) : (not' xs)
 dff init xs=init:xs
@@ -24,21 +28,31 @@ add a b = ((head a)+(head b)) : (add (tail a) (tail b))
 cnt  = dff 0 (add cnt  one)
 cnt2 = dff 0 (add cnt2 one)
 
+ram::Val->Val->Val->Val->Val->Val
 ram re radr we wadr wdat =
-  (if' (eq re (eq radr 0)) ram0 
-   (if' (eq re (eq radr 1)) ram1
-    (if' (eq re (eq radr 2)) ram2
-     (if' (eq re (eq radr 3)) ram3
+  (if' (eq re (eq radr (val 0))) ram0 
+   (if' (eq re (eq radr (val 1))) ram1
+    (if' (eq re (eq radr (val 2))) ram2
+     (if' (eq re (eq radr (val 3))) ram3
       zero
      )
     )
    )
   )
   where
-    ram0=dff 0 (if' (eq  we (eq wadr 0)) wdat ram0)
-    ram1=dff 0 (if' (eq  we (eq wadr 1)) wdat ram1)
-    ram2=dff 0 (if' (eq  we (eq wadr 2)) wdat ram2)
-    ram3=dff 0 (if' (eq  we (eq wadr 3)) wdat ram3)
+    ram0=dff 0 (if' (eq  we (eq wadr (val 0))) wdat ram0)
+    ram1=dff 0 (if' (eq  we (eq wadr (val 1))) wdat ram1)
+    ram2=dff 0 (if' (eq  we (eq wadr (val 2))) wdat ram2)
+    ram3=dff 0 (if' (eq  we (eq wadr (val 3))) wdat ram3)
+    
+wadr::Val
+radr::Val
+re  =[0,0,0,0,0,1,1,1,1,0,0]    
+we  =[1,1,1,1,0,0,0,0,0,0,0]    
+wadr=[0,1,2,3,0,0,0,0,0,0,0]    
+radr=[0,0,0,0,0,0,1,2,3,0,0]    
+wdat=[3,2,1,5,0,0,0,0,0,0,0]    
 
+rdat=ram re radr we wadr wdat
 main=do
-  print $ map (take 5) [cnt,cnt2,b]
+  print $ map (take 10) [rdat]
